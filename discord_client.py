106a1a73e6b62eebc58366ae59792a31f16beb80
Ahmed Sherif <69525619+Hiccupcc@users.noreply.github.com>
2023-08-19 16:52:59 +0300

    first commit

---
+++ discord_client.py
@@ -0,0 +1,153 @@
+from typing import Optional
+from datetime import datetime
+import discord
+from discord.ext import commands
+from discord.ui import *
+from steam_client import *
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Logged on as ", bot.user)
+
+
+@bot.command()
+async def user(ctx, args):
+    user = str(args)
+    steamAccount = steamgetuser(user, identifier=0)
+    profileMenu = ProfileMenuView(steamAccount, ctx)
+    embed = await summaryEmbed(steamAccount, ctx)
+    await ctx.send(embed=embed, view=profileMenu)
+
+
+# @bot.command()
+# async def searchuserid(ctx, args):
+#       user = str(args)
+#       steamAccount = await steamgetuser(user, identifier=1)
+#       profileMenu = ProfileMenuView(steamAccount, ctx)
+#       embed = await summaryEmbed(steamAccount, ctx)
+#       await ctx.send(embed=embed, view = profileMenu)
+
+
+@bot.command()
+async def game(ctx, *, args):
+    try:
+        game = str(args)
+        gameInformation = await steamgetgamepage(game, ctx)
+        gamePageMenu = GameMenuView(gameInformation, ctx)
+        embed = gameInformationEmbed(gameInformation)
+        await ctx.send(embed=embed, view=gamePageMenu)
+    except:
+        game = str(args)
+        availableGames = await steamgetgamepage(game, ctx)
+        embed = availableGamesEmbed(availableGames)
+        await ctx.send(embed=embed)
+
+
+class ProfileMenuView(discord.ui.View):
+    def __init__(self, steamAccount, ctx):
+        self.steamAccount = steamAccount
+        self.ctx = ctx
+        super().__init__()
+
+    @discord.ui.button(label="Owned Games", emoji="🎮")
+    async def games_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button):
+        newGamesPage = ownedGamesEmbed(self.steamAccount, self.ctx)
+        await interaction.response.edit_message(embed=newGamesPage)
+
+    @discord.ui.button(label="Profile Summary", emoji="🧭")
+    async def profile_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button):
+        newProfilePage = await summaryEmbed(self.steamAccount, self.ctx)
+        await interaction.response.edit_message(embed=newProfilePage)
+
+    @discord.ui.button(label="Friends List", emoji="🙎‍♂️")
+    async def friends_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button):
+        newfriendsPage = friendsEmbed(self.steamAccount, self.ctx)
+        await interaction.response.edit_message(embed=newfriendsPage)
+
+class GameMenuView(discord.ui.View):
+    def __init__(self, gameInformation, ctx):
+        self.gameInformation = gameInformation
+        self.ctx = ctx
+        super().__init__()
+    @discord.ui.button(label="Achievements", emoji="🏆")
+    async def achievements_Button(
+        self, interaction: discord.Interaction, button: discord.ui.Button):
+        showachievementsEmbed = achievementsEmbed(self.gameInformation)
+        await interaction.response.edit_message(embed=showachievementsEmbed)
+
+
+
+async def summaryEmbed(steamAccount, ctx):
+    embed = discord.Embed(title=steamAccount[0])
+    embed.set_thumbnail(url=steamAccount[1])
+    embed.add_field(name="Date Created", value=steamAccount[2], inline=True)
+    embed.add_field(name="Online Status", value=steamAccount[3], inline=True)
+    embed.add_field(name="Steam Level", value=f"{steamAccount[4]}", inline=True)
+    embed.add_field(
+        name="Number of Badges", value=len(steamAccount[6]["badges"]), inline=False
+    )
+    embed.set_author(name=steamAccount[5])
+
+    return embed
+
+
+def ownedGamesEmbed(steamAccount, ctx):
+    returnedOwnedGames = steamgetownedgames(steamAccount[0])
+    embed = discord.Embed(title=f"{steamAccount[5]}'s Games")
+    embed.set_thumbnail(url=steamAccount[1])
+    embed.set_footer(text=f"Number of owned games: {returnedOwnedGames[0]}")
+    for x in range(int(returnedOwnedGames[0])):
+        playtime = round(int(returnedOwnedGames[1][x]["playtime_forever"]) / 60)
+        embed.add_field(
+            name=returnedOwnedGames[1][x]["name"], value=f"{playtime} hours played."
+        )
+    return embed
+
+
+def friendsEmbed(steamAccount, ctx):
+    friendslist = steamgetfriends(steamAccount[0])
+    embed = discord.Embed(title=f"{steamAccount[5]}'s Friends")
+    embed.set_thumbnail(url=steamAccount[1])
+    for x in range(int(len(friendslist["friends"]))):
+        friend_since = datetime.fromtimestamp(
+            int(friendslist["friends"][x]["friend_since"])
+        )
+        embed.add_field(
+            name=friendslist["friends"][x]["personaname"],
+            value=f"Friends since: {friend_since}",
+        )
+    return embed
+
+
+def gameInformationEmbed(gameInformation):
+    embed = discord.Embed(title=gameInformation[0])
+    embed.add_field(name="Description", value=gameInformation[1], inline=True)
+    embed.add_field(name="Price", value=gameInformation[4], inline=False)
+    embed.set_image(url=gameInformation[2])
+    return embed
+
+
+def availableGamesEmbed(availableGames):
+    embed = discord.Embed(title="Available Games")
+    numbering = 1
+    for game in availableGames:
+        embed.add_field(name="", value=f"{numbering}: {game}", inline=False)
+        numbering += 1
+    return embed
+
+def achievementsEmbed(gameInfromation):
+   embed = discord.Embed(title="Available Achievements")
+   embed.set_footer(text=f"Number of achievements: {gameInfromation[5]['total']}")
+   embed.add_field(name="", value="")
+   return embed
+
+bot.run("TOKEN")
+
---
+++ steam_client.py
@@ -0,0 +1,108 @@
+from steam import Steam
+from decouple import config
+from datetime import datetime
+
+steam = Steam("F661D16714DC5E358FFCF59EAC4FAD17")
+
+errorMessage = "User not found."
+
+def steamgetuser(user, identifier):
+  print("Fetching Account...")
+  
+  if identifier == 0:
+    userdetails = dict(steam.users.search_user(user))
+  if identifier == 1:
+    userdetails = dict(steam.users.get_user_details(user))
+  
+
+  steamid = userdetails["player"]["steamid"]
+  avatarfull = userdetails["player"]["avatarfull"]
+  creation_timestamp = userdetails["player"]["timecreated"]
+  persona_state = userdetails["player"]["personastate"]
+  persona_name = userdetails["player"]["personaname"]
+  online_status = ""
+  badges = steam.users.get_user_badges(steamid)
+  creation_date = datetime.fromtimestamp(creation_timestamp)
+
+  match persona_state:
+    case 0:
+      online_status = "Offline"
+    case 1:
+      online_status = "Online"
+    case 2:
+      online_status = "Busy"
+    case 3:
+      online_status = "Away"
+    case 4:
+      online_status = "Snooze"
+    case 5:
+      online_status = "Looking to Trade"
+    case 6:
+      online_status = "Looking to Play"
+
+  level = steam.users.get_user_steam_level(steamid)
+  #{'player_level': 16}
+  level = str(level).replace("{'player_level': ", "").replace("}", "")
+  #16   
+  steamAccountInfo = [steamid, avatarfull, creation_date, online_status, level, persona_name, badges]
+  print("Retrieved Account.")
+  return steamAccountInfo
+
+
+def steamgetownedgames(id):
+  print("Fetching owned games...")
+  gamesDict = steam.users.get_owned_games(id)
+  numberofOwnedGames = gamesDict["game_count"]
+  games = gamesDict["games"]
+
+  ownedGamesInformation = [numberofOwnedGames, games]
+  print("Retrieved Games.")
+  return ownedGamesInformation
+
+def steamgetfriends(id):
+  print("Fetching friends list...")
+  friendslist = dict(steam.users.get_user_friends_list(id))
+  print("Retrieved Friends List.")
+  return friendslist
+
+async def steamgetgamepage(game, ctx):
+  gameSearch = dict(steam.apps.search_games(game))
+  print(gameSearch)
+  for x in range(len(gameSearch["apps"])):
+    print(gameSearch["apps"][x])
+    print(game)
+    apiGame = gameSearch["apps"][x]["name"].lower().replace("\\u2122", "").replace("\\u00ae","").replace("\\u2019", "'")
+    if apiGame == game.lower():
+      gameID = gameSearch["apps"][x]["id"]
+      try:
+       gamePrice = gameSearch["apps"][x]["price"]
+       if gamePrice == "" :
+        gamePrice = "Unspecified"
+        return
+      except: 
+        gamePrice = "Unspecified"
+        return
+     
+
+       
+
+
+  
+
+  try:
+   gamePage = steam.apps.get_app_details(gameID)
+   gameName = gamePage[str(gameID)]["data"]["name"]
+   gameAchievements = gamePage[str(gameID)]["data"]["achievements"]
+   gameDescription = gamePage[str(gameID)]["data"]["short_description"]
+   gameImage = gamePage[str(gameID)]["data"]["header_image"]
+   gameInformation = [gameName, gameDescription, gameImage, gameSearch, gamePrice, gameAchievements]
+   return gameInformation
+  except:
+   availableGames = []
+   for x in range(len(gameSearch["apps"])):
+    apiGame = gameSearch["apps"][x]["name"].replace("\\u2122", "").replace("\\u00ae","").replace("\\u2019", "'")
+    availableGames.append(apiGame)
+   if len(availableGames) != 0:
+    return availableGames
+
+
